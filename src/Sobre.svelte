eb5d94f98a39cf0a73097173ce21b39d96ecae4a
lazhenri <lhenriqueqoliveira@gmail.com>
2022-07-08 12:19:22 -0300

    criando repositorio svelte

---
+++ src/Sobre.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+	<link rel="stylesheet" href="/styles/sobre.css">
+</svelte:head>
+
+<script>
+	import VoltarMenu from './VoltarMenu.svelte'
+</script>
+
+<h1>Sobre</h1>
+
+<img src="images/logoIFPE.png" alt="logo IPFE"/>
+
+<p>
+		Allan Diego Silva Lima - <a href="allan.lima@igarassu.ifpe.edu.br">allan.lima@igarassu.ifpe.edu.br</a>
+</p>
+
+<VoltarMenu/>
